f76950aa7bd5eaae808fab4ec55f1bcd3fcaacbe
Collin York <cyork@ipro.org>
2026-04-09 12:56:52 -0400

    test commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello World from Cloudflare Container!', {
+    return new Response('Hello World from Cloudflare Container!!!!!!', {
       headers: {
         'content-type': 'text/plain',
       },
